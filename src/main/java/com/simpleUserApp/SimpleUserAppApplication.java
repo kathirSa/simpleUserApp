901d84b71b8939b3c54721d2c806cd412f6c32fe
sankat_gmx <sanje.ka@gmx.de>
2021-05-22 13:09:49 +0200

    test

---
+++ src/main/java/com/simpleUserApp/SimpleUserAppApplication.java
@@ -0,0 +1,13 @@
+package com.simpleUserApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleUserAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleUserAppApplication.class, args);
+	}
+
+}
